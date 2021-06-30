a7b3f4d236773c33d7ccc363619e46bc9f26b127
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2021-06-30 18:35:07 +0900

    fix unlink directory

--- scripts/create_link.py
+++ scripts/create_link.py
@@ -2,11 +2,15 @@
 
 from os import path
 from pathlib import Path
+import shutil
 
 
 def create_link(target: Path, link_from: Path):
     # Unlink old symbolic link
-    link_from.unlink(missing_ok=True)
+    if link_from.is_dir():
+        shutil.rmtree(link_from)
+    if link_from.exists():
+        link_from.unlink(missing_ok=True)
 
     # Create symbolic link
     link_from.symlink_to(target=target, target_is_directory=True)
