9bfc4f419b611d2fe6cb4e5fb38404aa22fb6d01
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2021-07-02 21:08:06 +0900

    fixup

--- scripts/util.py
+++ scripts/util.py
@@ -13,26 +13,30 @@ def dir_path(input):
         raise NotADirectoryError(input)
 
 
-def run_command(args: list) -> bool:
+def run_command(args: list, cwd: Path = None) -> bool:
     try:
-        subprocess.run(args=args, check=True, stdout=sys.stdout, stderr=sys.stderr)
+        subprocess.run(
+            args=args, check=True, stdout=sys.stdout, stderr=sys.stderr, cwd=cwd
+        )
     except CalledProcessError:
         return False
     return True
 
 
-def run_command_pipe(args: list) -> str:
+def run_command_pipe(args: list, cwd: Path = None) -> str:
     try:
-        proc = subprocess.run(args=args, check=True, capture_output=True, text=True)
+        proc = subprocess.run(
+            args=args, check=True, capture_output=True, text=True, cwd=cwd
+        )
         return proc.stdout
     except CalledProcessError:
         return ""
 
 
-def run_command_redirect(args: list, output_file: Path) -> bool:
+def run_command_redirect(args: list, output_file: Path, cwd: Path = None) -> bool:
     try:
         with open(output_file, "w") as out:
-            subprocess.run(args=args, check=True, stdout=out)
+            subprocess.run(args=args, check=True, stdout=out, cwd=cwd)
     except CalledProcessError:
         return False
     return True
