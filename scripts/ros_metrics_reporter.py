6917a4e9ec90160baaebd8d749dc0345b05b0b90
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2021-07-02 18:13:04 +0900

    fix path

--- scripts/ros_metrics_reporter.py
+++ scripts/ros_metrics_reporter.py
@@ -33,14 +33,14 @@ def ros_metrics_reporter(args):
     lizard_dir = args.output_dir / "lizard_result" / args.timestamp
     lizard_all(
         base_dir=args.base_dir,
-        output_dir=args.output_dir,
+        output_dir=lizard_dir,
         gh_action_dir=args.action_dir,
         ccn=args.ccn,
         nloc=args.nloc,
         arguments=args.arguments,
     )
     lizard_package(
-        output_dir=args.output_dir,
+        output_dir=lizard_dir,
         gh_action_dir=args.action_dir,
         exclude=exclude,
         ccn=args.ccn,
@@ -50,23 +50,24 @@ def ros_metrics_reporter(args):
 
     # Scraping
     metrics_dir = args.output_dir / "metrics" / args.timestamp
+    metrics_dir.mkdir(parents=True, exist_ok=True)
     scraping(lcov_dir=lcov_dir, lizard_dir=lizard_dir, output_dir=metrics_dir)
 
     # Create symbolic link
-    lcov_latest_dir = lcov_dir = args.output_dir / "lcov_result" / "latest"
+    lcov_latest_dir = args.output_dir / "lcov_result" / "latest"
     lizard_latest_dir = args.output_dir / "lizard_result" / "latest"
     metrics_latest_dir = args.output_dir / "metrics" / "latest"
 
-    create_link(target=lcov_dir, link_from=lcov_latest_dir)
-    create_link(target=lizard_dir, link_from=lizard_latest_dir)
-    create_link(target=metrics_dir, link_from=metrics_latest_dir)
+    create_link(target=Path(args.timestamp), link_from=lcov_latest_dir)
+    create_link(target=Path(args.timestamp), link_from=lizard_latest_dir)
+    create_link(target=Path(args.timestamp), link_from=metrics_latest_dir)
 
     # Create static page
     hugo_template_dir = args.action_dir / "template" / "hugo"
     tidy_result_dir = args.output_dir / "tidy-reports" / args.timestamp
 
     create_static_page(
-        input_dir=metrics_dir,
+        input_dir=metrics_dir.parent,
         hugo_root_dir=args.hugo_root_dir,
         hugo_template_dir=hugo_template_dir,
         lcov_result_path=lcov_latest_dir,
