e89c511741aec69962729e294d2a1b3c53cdb7e2
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2021-12-02 10:56:29 +0900

    change error handling

--- ros_metrics_reporter/util.py
+++ ros_metrics_reporter/util.py
@@ -36,13 +36,10 @@ def run_command(args: List[str], cwd: Path = None) -> bool:
 
 
 def run_command_pipe(args: List[str], cwd: Path = None) -> str:
-    try:
-        proc = subprocess.run(
-            args=args, check=True, capture_output=True, text=True, cwd=cwd
-        )
-        return proc.stdout
-    except CalledProcessError:
-        return ""
+    proc = subprocess.run(
+        args=args, check=True, capture_output=True, text=True, cwd=cwd
+    )
+    return proc.stdout
 
 
 def run_command_redirect(args: list, output_file: Path, cwd: Path = None) -> bool:
