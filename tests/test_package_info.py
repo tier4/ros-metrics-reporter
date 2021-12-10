4fd767f642013e35522705304a90bc54c8a237e4
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2021-12-10 17:10:12 +0900

    fix pytest
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>

--- tests/test_package_info.py
+++ tests/test_package_info.py
@@ -1,19 +1,37 @@
 from pathlib import Path
 from ros_metrics_reporter.package_info import *
+import shutil
 
 
-def test_package_info():
-    package_info = PackageInfo(ros_ws=Path("example/src/demos"))
-    assert package_info.ros_ws == Path("example/src/demos").absolute()
+def test_package_info_root(tmp_path):
+    dummy_pkg = Path(os.path.dirname(__file__), "resource", "dummy_package")
+    ros_ws = Path(tmp_path)
+    git_ws = Path(tmp_path)
+    (git_ws / ".git").mkdir(parents=True)
+    shutil.copytree(dummy_pkg, git_ws / "dummy_package")
 
-    package = package_info.get_package_info("demo_nodes_cpp")
-    assert package.name == "demo_nodes_cpp"
-    assert package.path == Path("demo_nodes_cpp")
+    package_info = PackageInfo(ros_ws)
+    assert package_info.ros_ws == Path(tmp_path)
+
+    package = package_info.get_package_info("dummy_package")
+    assert package.name == "dummy_package"
+    assert package.git_ws == git_ws
+    assert package.path == "dummy_package"
     assert package.type == "(ros.ament_cmake)"
 
-    package_count = 0
-    for package in package_info:
-        package_count += 1
-        assert package.name is not None
 
-    assert len(package_info.package_list) == package_count
+def test_package_info_subdir(tmp_path):
+    dummy_pkg = Path(os.path.dirname(__file__), "resource", "dummy_package")
+    ros_ws = Path(tmp_path)
+    git_ws = Path(tmp_path / "subdir")
+    (git_ws / ".git").mkdir(parents=True)
+    shutil.copytree(dummy_pkg, git_ws / "dummy_package")
+
+    package_info = PackageInfo(ros_ws)
+    assert package_info.ros_ws == Path(tmp_path)
+
+    package = package_info.get_package_info("dummy_package")
+    assert package.name == "dummy_package"
+    assert package.git_ws == git_ws
+    assert package.path == "dummy_package"
+    assert package.type == "(ros.ament_cmake)"
