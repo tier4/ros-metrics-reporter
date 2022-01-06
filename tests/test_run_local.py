fba17e94108311b22c88c2f975160f79998643e0
Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>
2022-01-06 12:46:20 +0900

    change build flag
    Signed-off-by: Keisuke Shima <19993104+KeisukeShima@users.noreply.github.com>

--- tests/test_run_local.py
+++ tests/test_run_local.py
@@ -21,7 +21,7 @@ def setup():
 
 def build():
     res = subprocess.run(
-        '. /opt/ros/galactic/setup.sh && colcon build --symlink-install --cmake-args -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage -DCOVERAGE_RUN=1" -DCMAKE_C_FLAGS="-fprofile-arcs -ftest-coverage -DCOVERAGE_RUN=1" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --mixin coverage-gcc',
+        '. /opt/ros/galactic/setup.sh && colcon build --symlink-install --cmake-args -DCMAKE_CXX_FLAGS="-fprofile-arcs -ftest-coverage --coverage -DCOVERAGE_RUN=1" -DCMAKE_C_FLAGS="-fprofile-arcs --coverage -ftest-coverage -DCOVERAGE_RUN=1" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON',
         shell=True,
         cwd=ros_ws,
     )
